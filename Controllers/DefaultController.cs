1d305cb33aaa3980091d3d3bd7abc9f2ba23d007
trkhmrt <trkhamarat@gmail.com>
2023-09-17 01:27:56 +0300

    Update DefaultController.cs

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -29,6 +29,59 @@ namespace WEP_API.Controllers
 
             return Ok();
         }
+        [HttpGet("{id}")]
+
+        public IActionResult GetEmployee(int id)
+        {
+            using var c = new Context();
+            var employee = c.Employees.Find(id);
+            if(employee==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(employee);
+            }
+
+        }
+        [HttpGet("{id}")]
+
+        public IActionResult EmployeeDelete(int id)
+        {
+            using var c = new Context();
+            var employee = c.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                c.Remove(id);
+                c.SaveChanges();
+                return Ok(employee);
+            }
+
+        }
+        [HttpPut]
+
+        public IActionResult EmployeeUpdate(Employee employee)
+        {
+            using var c = new Context();
+            var emp = c.Find<Employee>(employee.EmployeeID);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                emp.Name = employee.Name;
+                c.Update(emp);
+                c.SaveChanges();
+                return Ok();
+            }
+
+        }
     }
 }
 
